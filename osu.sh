103de54c928b33d8cf741027b31114ef4b23c3e0
chent7 <34766434+chent7@users.noreply.github.com>
2022-06-22 23:25:43 +0800

    Add files via upload
    main script to run osu

---
+++ osu.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export vblank_mode=0
+export WINEPREFIX="$HOME/.osu-wine"
+export WINEARCH=win32
+export PIPEWIRE_LATENCY=128/48000
+
+pgrep -x winediscordipcbridge.exe > /dev/null || wine "$HOME/.resources/osu!/winediscordipcbridge.exe" &
+
+sleep 1
+wine "/run/media/chent7/Fast Storage/Games/osu!/osu!.exe" $@
