1ad9fd31c136563f58a4d6f3a6541824ec2589ed
chent7 <34766434+chent7@users.noreply.github.com>
2022-06-22 23:48:48 +0800

    Update osu.sh

--- osu.sh
+++ osu.sh
@@ -3,7 +3,7 @@
 export vblank_mode=0
 export WINEPREFIX="$HOME/.osu-wine"
 export WINEARCH=win32
-export PIPEWIRE_LATENCY=128/48000
+export PIPEWIRE_LATENCY=128/48000 # for pipewire latency (obviously), much better than pulseaudio
 
 pgrep -x winediscordipcbridge.exe > /dev/null || wine "$HOME/.resources/osu!/winediscordipcbridge.exe" &
 
